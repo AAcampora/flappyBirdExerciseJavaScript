16f53d1738ef28d131d90cda811bbc96fffc0b16
Alessio Acampora <acampora199@gmail.com>
2023-02-15 11:30:25 +0000

    added flap functionality

--- src/index.js
+++ src/index.js
@@ -8,25 +8,50 @@ const config = {
   height: 600,
   physics:{
     //arcade physics plugin handles physics simulation
-    default:'arcade'
+    default:'arcade',
+    arcade:{
+      gravity: {y:400},
+      debug: true,
+    }
   },
   scene: {
-    preload: preload,
-    create: create,
+    preload,
+    create,
+    update
   }
 }
+
+//game variables
+const FLAPSTRENGHT = -300;
+
+//game objects
+let bird = null;
+let totalDelta = null;
 
 // Loading assets, such as images, music, etc etc...
 function preload() {
   // 'this' context - scene
   //contains functions and properties we can use
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
 }
 
 function create() {
 
   //background image
-  this.add.image(config.width/2, config.height/2, 'sky')
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  //the bird sprite
+  bird = this.physics.add.sprite(config.width * 0.1,  config.height / 2, 'bird').setOrigin(0);
+
+  this.input.keyboard.on('keydown-SPACE', Flap);
+}
+
+function update(time, delta) {
+
+}
+
+function Flap(){
+  bird.body.velocity.y = FLAPSTRENGHT;
 }
 
 
